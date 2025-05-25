6345ada1abf88530f5087a6fdc603030772526b1
Alexander Pletnev <pletnev.a.a@yandex.ru>
2025-05-25 22:11:53 +0200

    conditionals and loops

--- Playlist/TraversyMedia/04_conditionals.php
+++ Playlist/TraversyMedia/04_conditionals.php
@@ -23,6 +23,31 @@ if (condition) {
   // code to be executed if condition is true
 }
 */
+// $age = 18;
+// if ($age >= 18) {
+//     echo 'You are old enough to vote';
+// } else {
+//     echo 'Sorry, you are not old enough to vote';
+// }
+
+$some_date = date("H"); // Get the current hour in 24-hour format
+// echo $some_date . '<br>';
+// if ($some_date < 12) {
+//     echo 'Good morning!';
+// } elseif ($some_date < 18) {
+//     echo 'Good afternoon!';
+// } else {
+//     echo 'Good evening!';
+// }
+
+$posts = ['First Post'];
+
+// if (!empty($posts)) {
+//     echo $posts[0];
+// } else {
+//     echo 'No posts';
+// }
+
 
 /* -------- Ternary Operator -------- */
 /*
@@ -31,5 +56,26 @@ if (condition) {
     condition ? true : false;
 */
 
+// echo !empty($posts) ? $posts[0] : 'No posts'; // Outputs 'First Post' if $posts is not empty, otherwise outputs 'No posts'
+// $first_post = !empty($posts) ? $posts[0] : 'No posts'; // Assigns 'First Post' to $first_post if $posts is not empty, otherwise assigns 'No posts'
+// $first_post = $posts[0] ?? 'No posts'; // Null coalescing operator, assigns 'First Post' if $posts[0] exists, otherwise assigns 'No posts'
+// echo $first_post; // Outputs 'First Post'
+
 
 /* -------- Switch Statements ------- */
+
+$favcolor = 'onion ';
+switch ($favcolor) {
+    case 'red':
+        echo 'Your favorite color is red';
+        break;
+    case 'blue':
+        echo 'Your favorite color is blue';
+        break;
+    case 'green':
+        echo 'Your favorite color is green';
+        break;
+    default:
+        echo 'Your favorite color is neither red, blue, nor green';
+        break;
+};
---
+++ Playlist/TraversyMedia/05_loops.php
@@ -0,0 +1,65 @@
+<?php
+
+/* -------- Loops & Iteration ------- */
+
+/* ------------ For Loop ------------ */
+
+/*
+** For Loop Syntax
+    for (initialize; condition; increment) {
+    // code to be executed
+    }
+    
+*/
+
+// for ($i = 0; $i < 10; $i++) {
+//     echo 'Number i ' . $i . '<br>';
+// };
+
+
+/* ------------ While Loop ------------ */
+
+/*
+** While Loop Syntax
+    while (condition) {
+    // code to be executed
+    }
+*/
+// $i = 1; // Initialize the counter variable
+// while ($i < 10) {
+//     echo 'Number ' . $i . '<br>';
+//     $i++; // or $i = $i + 1; // Increment the counter variable
+// };
+
+
+/* ---------- Do While Loop --------- */
+
+/*
+** Do While Loop Syntax
+    do {
+    // code to be executed
+    } while (condition);
+
+do...while loop will always execute the block of code once, even if the condition is false.
+*/
+// $i = 1; // Initialize the counter variable
+// do {
+//     echo 'Number ' . $i . '<br>';
+//     $i++; // Increment the counter variable
+// } while ($i < 20); // The loop will continue as long as $i is less than 20
+
+
+
+/* ---------- Foreach Loop ---------- */
+
+/*
+** Foreach Loop Syntax
+    foreach ($array as $value) {
+    // code to be executed
+    }
+*/
+
+$posts = ['First Post', 'Second Post', 'Third Post'];
+foreach ($posts as $post) {
+    echo $post . '<br>';
+}
