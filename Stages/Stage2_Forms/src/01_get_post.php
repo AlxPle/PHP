f66247b044cc7f0e969fe573de8b82138d68078b
AlxPle <pletnev1990.a.a@gmail.com>
2026-02-22 11:32:09 +0100

    GET/POST info

---
+++ Stages/Stage2_Forms/src/01_get_post.php
@@ -0,0 +1,12 @@
+<?php
+// Print all GET parameters
+echo 'GET Parameters: <br>';
+print_r($_GET); // Shows URL parameters
+
+// Print all POST parameters
+echo '<br>POST Parameters: <br>';
+print_r($_POST); // Shows form data
+
+// Print server info
+echo '<br>Server Info: <br>';
+print_r($_SERVER); // Shows server/request info
